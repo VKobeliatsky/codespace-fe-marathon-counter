9a35d0533379644cb64fd955eb5076c571d870ea
Volodymyr Kobeliatsky <volodymyrk@wix.com>
2018-07-25 14:48:21 +0300

    added react-redux types; fixed type declarations error;

--- src/playground.ts
+++ src/playground.ts
@@ -39,9 +39,9 @@ function error(message: string): never {
     throw new Error(message);
 }
 
-function fail() {
-    return error("Something failed");
-}
+// function fail() {
+//     return error("Something failed");
+// }
 
 function infiniteLoop(): never {
     while (true) {
